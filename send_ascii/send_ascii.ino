81921b872e0e308b292706dc56c2b0c7d6229f56
Christian Zufferey <christian@zufferey.com>
2024-10-31 17:35:36 +0100

    refact: nettoyé le code

--- send_ascii/send_ascii.ino
+++ send_ascii/send_ascii.ino
@@ -1,6 +1,6 @@
 // envoie un caractère ascii sur le 2e port UART afin de pouvoir le 'voir' avec un oscilloscope
 
-// zf220727.1634, zf241031.1644
+// zf220727.1634, zf241031.1735
 
 // Sources:
 // https://seeeddoc.github.io/Grove-Base_shield_v2/res/Base_Shield_v2.pdf
@@ -17,27 +17,19 @@ SoftwareSerial mySerial(0, 3); // RX, TX
 String zString = "toto";
 
 
-
-
 void setup() {
   Serial.begin(9600); // open a serial connection to your computer
   mySerial.begin(9600);
   delay(1000);
   Serial.println("\n\ncoucou\n");
-
 }
 
 void loop() {
-  
-  
   zString = "D";
 
   // print out the value to the Serial Monitor
   Serial.println(zString);
   mySerial.print(zString);
-
-
-
 
   // wait 
   delay(1550);
